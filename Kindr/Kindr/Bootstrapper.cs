99c9e4eec2deb8a5472d519d66ac0fc5e3be0132
Adam Prentice <adamjlprentice@gmail.com>
2017-11-24 09:12:42 +0000

    Initial Nancy Site

---
+++ Kindr/Kindr/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace Kindr
+{
+    public class Bootstrapper  : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            StaticConfiguration.DisableErrorTraces = false;
+        }
+    }
+}
---
+++ Kindr/Kindr/Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+namespace Kindr.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule() : base("/")
+        {
+            Get["/"] = x => GetHome();
+        }
+
+        public dynamic GetHome()
+        {
+            return this.View["Home"];
+        }
+    }
+}
