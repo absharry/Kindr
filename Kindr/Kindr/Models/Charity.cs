db9b20ca374b96be696842781934151271c52650
Julia Harkins <398905@edu.cwa.ac.uk>
2017-11-24 16:13:52 +0000

    Created Results page!

---
+++ Kindr/Kindr/Models/Charity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kindr.Models
+{
+    public class Charity
+    {
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ Kindr/Kindr/Modules/CharityResultsModule.cs
@@ -0,0 +1,31 @@
+﻿using Kindr.Models;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kindr.Modules
+{
+    public class CharityResultsModule : NancyModule
+    {
+        public CharityResultsModule()
+        {
+            Get["/results"] = x => GetResults();
+        }
+
+        public dynamic GetResults()
+        {
+            var results = this.GetResultsData();
+            return this.View["CharityResults"].WithModel(results);
+        }
+
+        private CharityResultsModel GetResultsData()
+        {
+            var results = new CharityResultsModel();
+            results.Charities.Add(new Charity { Name = "Dogs Trust"});
+
+            return results;
+        }
+    }
+}
