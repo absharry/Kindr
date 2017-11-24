ee852188905d7efe62c17b5779069fbf421f1fed
Unknown <shaunsupra@googlemail.com>
2017-11-24 20:51:24 +0000

    Finished (I think) business login and charity details

--- Kindr/Kindr/Bootstrapper.cs
+++ Kindr/Kindr/Bootstrapper.cs
@@ -33,7 +33,9 @@ namespace Kindr
                 new TagModel() { },
             })
             {
-                Name = "Dogs Trust",Id=0,Description="Helps Dogs",ImageUrl= "https://pbs.twimg.com/profile_images/931550545860616199/hEit2Niu.jpg"
+                Name = "Dogs Trust",Id=0,
+                Description = "Dogs Trust, formerly known as the National Canine Defence League, is an animal welfare charity and humane society in the United Kingdom which specialises in the well-being of dogs.",
+                ImageUrl= "https://pbs.twimg.com/profile_images/931550545860616199/hEit2Niu.jpg"
             });
 
             models.Add(new CharityModel(new List<TagModel>()
