1a77d405152a204d4162cae9c6d6ba9d19b33c4b
Unknown <simon@simonjohnson.me>
2017-11-25 14:29:41 +0000

    Dashboard.

---
+++ Kindr/Kindr/Models/DashboardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kindr.Models
+{
+    public class DashboardModel
+    {
+        public List<CharityModel> ActiveCharities { get; set; } = new List<CharityModel>();
+
+        public List<CharityModel> SuggestedCharities { get; set; } = new List<CharityModel>();
+    }
+}
--- Kindr/Kindr/Modules/HomeModule.cs
+++ Kindr/Kindr/Modules/HomeModule.cs
@@ -13,6 +13,7 @@ namespace Kindr.Modules
             Get["/"] = x => GetHome();
             Get["/business_login"] = x => GetBusinessLogin();
             Get["/charity_details/{id}"] = x => GetCharityDetails(x.id);
+            Get["/dashboard"] = x => GetDashboard();
         }
 
         public dynamic GetHome()
@@ -31,6 +32,24 @@ namespace Kindr.Modules
             ViewBag.Title = "Charity Details";
             var model = GetResultsData(id);
             return this.View["CharityDetails"].WithModel(model);
+        }
+
+        public dynamic GetDashboard()
+        {
+            var model = GetResultsData();
+
+            var newModel = new DashboardModel()
+            {
+                ActiveCharities = model.Take(2).ToList(),
+                SuggestedCharities = model.Skip(2).ToList()
+            };
+
+            return this.View["Dashboard"].WithModel(newModel);
+        }
+
+        private List<CharityModel> GetResultsData()
+        {
+            return (List<CharityModel>)HttpContext.Current.Application["CharityModels"];
         }
 
         private CharityModel GetResultsData(int id)
